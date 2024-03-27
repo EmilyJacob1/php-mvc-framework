752a162459979b45da561c563eaf1a1c4ffedb37
emily <emily.jacob.nl@gmail.com>
2024-03-27 12:18:58 +0100

    x

--- public/components/head.php
+++ public/components/head.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>movies</title>
+    <title>template</title>
     <!--styles-->
     <link rel="stylesheet" href="/fireArcade/public/assets/css/main.css">
     <!--bootstrap-->
